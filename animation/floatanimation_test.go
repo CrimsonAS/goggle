62b2d4e3fd4e35491edb1ecedda836f7396b2f69
Robin Burchell <robin.burchell@crimson.no>
2017-12-02 19:58:00 +0100

    Add benchmark for FloatAnimation

---
+++ animation/floatanimation_test.go
@@ -0,0 +1,30 @@
+package animation
+
+import (
+	"testing"
+	"time"
+)
+
+var floatAnimationBench *FloatAnimation
+var floatBench float32
+
+func BenchmarkConstruct(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		floatAnimationBench = &FloatAnimation{
+			From:     0,
+			To:       1000,
+			Duration: 1000 * time.Millisecond,
+		}
+	}
+}
+
+func BenchmarkGet(b *testing.B) {
+	anim := FloatAnimation{
+		From:     0,
+		To:       1000,
+		Duration: 1000 * time.Millisecond,
+	}
+	for i := 0; i < b.N; i++ {
+		floatBench = anim.Get()
+	}
+}
